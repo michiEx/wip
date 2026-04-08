76ce2c2778b64a71f0a0a5b948c5ad0b7a052c28
michiEx <108243447+michiEx@users.noreply.github.com>
2026-04-09 08:51:41 +0900

    awaji2026 upload

---
+++ awaji2026/packages/js/app.js
@@ -0,0 +1,17 @@
+$(document).foundation();
+
+/*
+$(document).ready(function(){
+	resizeDiv();
+});
+
+window.onresize = function(event) {
+	resizeDiv();
+}
+
+function resizeDiv() {
+	vpw = $(window).width();
+	vph = $(window).height();
+	$('header').css({'height': vph + 'px'});
+}
+*/
